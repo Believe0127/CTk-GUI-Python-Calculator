87b93a192e2fd22a9c8edaa1aaf8ce8810ed4649
gingingila <101379299+Believe0127@users.noreply.github.com>
2025-03-28 01:26:28 +0900

    Update Calculator.py
    Cleaned up messy code

--- Calculator.py
+++ Calculator.py
@@ -1,61 +1,113 @@
-import func
 import customtkinter as CTk
 
-CTk.set_appearance_mode('Dark')
-CTk.set_default_color_theme('dark-blue')
+#def main() -> None:
+class Calculator(CTk.CTk):
+    def __init__(self):
+        super().__init__()
 
-app = CTk.CTk()
-app.title("電卓 - Ver.5.6")
-app.geometry("299x330")
-app.iconbitmap(default="data/icon.ico")
-app.resizable(0,0)
+        self.rendering_items = [
+           'AC','%','C','+'
+           ,'7','8','9','×'
+           ,'4','5','6','ー'
+           ,'1','2','3','÷'
+           ,'00','0','.','='
+        ]
 
-write_entry = CTk.CTkEntry(master=app, state="readonly", placeholder_text="0", placeholder_text_color="gray", height=50, width=250, bg_color='gray', font=("Segoe UI", 19), corner_radius=2, border_color="#22a6e3")
-write_entry.pack()
-write_entry.place(x=25, y=20)
+        self.title("電卓")
+        self.geometry("299x330")
+        self.iconbitmap(default="data/icon.ico")
+        self.resizable(0, 0)
+        
+        self.Setup_UI()
 
-zero_btn = CTk.CTkButton(master=app, text="0", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("0"))
-zerozero_btn = CTk.CTkButton(master=app, text="00", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("00"))
-one_btn = CTk.CTkButton(master=app, text="1", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("1"))
-two_btn = CTk.CTkButton(master=app, text="2", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("2"))
-three_btn = CTk.CTkButton(master=app, text="3", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("3"))
-four_btn = CTk.CTkButton(master=app, text="4", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("4"))
-five_btn = CTk.CTkButton(master=app, text="5", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("5"))
-six_btn = CTk.CTkButton(master=app, text="6", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("6"))
-seven_btn = CTk.CTkButton(master=app, text="7", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("7"))
-eight_btn = CTk.CTkButton(master=app, text="8", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("8"))
-nine_btn = CTk.CTkButton(master=app, text="9", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("9"))
+    def Setup_UI(self):
+        CTk.set_default_color_theme('dark-blue')
+        CTk.set_appearance_mode('Dark')
 
-zero_btn.place(x=85, y=270)
-zerozero_btn.place(x=20, y=270)
-one_btn.place(x=20, y=225)
-two_btn.place(x=85, y=225)
-three_btn.place(x=150, y=225)
-four_btn.place(x=20, y=180)
-five_btn.place(x=85, y=180)
-six_btn.place(x=150, y=180)
-seven_btn.place(x=20, y=135)
-eight_btn.place(x=85, y=135)
-nine_btn.place(x=150, y=135)
+        self.write_entry = CTk.CTkEntry(
+            master=self, state="readonly", placeholder_text="0", placeholder_text_color="gray", height=50, 
+            width=250, bg_color='gray', font=("Segoe UI", 19), corner_radius=2, border_color="#50b4f0"
+        )
+        self.write_entry.place(x=25, y=20)
+      
+        posX = 20
+        posY = 90
+        for char in self.rendering_items:
+            if char == "AC":
+                CTk.CTkButton(
+                    master=self, text="AC", height=45, width=65, font=("Segoe UI", 28),
+                    hover_color="#484848", fg_color="#383838", command=self.AllClear
+                ).place(x=posX, y=posY)
+            elif char == "C":
+                CTk.CTkButton(
+                    master=self, text="C", height=45, width=65, font=("Segoe UI", 28),
+                    hover_color="#484848", fg_color="#383838", command=self.Clear
+                ).place(x=posX, y=posY)
+            elif char == "%":
+                CTk.CTkButton(
+                    master=self, text="%", height=45, width=65, font=("Segoe UI", 28),
+                    hover_color="#484848", fg_color="#383838", command=self.percent_calculate
+                ).place(x=posX, y=posY)
+            elif char == "=":
+                CTk.CTkButton(
+                    master=self, text="=", height=45, width=65, font=("Segoe UI", 28),
+                    hover_color="#484848", fg_color="#383838", command=self.Calculate
+                ).place(x=posX, y=posY)
+            else:
+                CTk.CTkButton(
+                    master=self, text=char, height=45, width=65, font=("Segoe UI", 28),
+                    hover_color="#484848", fg_color="#383838", command=lambda char=char: self.write(char)
+                ).place(x=posX, y=posY)
+            
+            posX += 65
+            if (posX > 215):
+                posY += 45
+                
+                posX = 20 # 初期位置に戻す
 
-AC_btn = CTk.CTkButton(master=app, text="AC", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=func.AC)
-C_btn = CTk.CTkButton(master=app, text="C", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838",command=func.C)
-percent_btn = CTk.CTkButton(master=app, text="%", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=func.percent_calculate)
-plus_btn = CTk.CTkButton(master=app, text='+', height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("+"))
-minus_btn = CTk.CTkButton(master=app, text='ー', height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("-"))
-multiplication_btn = CTk.CTkButton(master=app, text='×', height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("×"))
-divide_btn = CTk.CTkButton(master=app, text="÷", height=45, width=65, font=("Segoe UI", 26), hover_color="#484848", fg_color="#383838", command=lambda:func.write("÷"))
-konma_btn = CTk.CTkButton(master=app, text=".", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=lambda:func.write("."))
-equal_btn = CTk.CTkButton(master=app, text="=", height=45, width=65, font=("Segoe UI", 28), hover_color="#484848", fg_color="#383838", command=func.Calculate)
+    def Clear(self):
+        self.write_entry.configure(state="normal")
 
-AC_btn.place(x=20, y=90)
-C_btn.place(x=150, y=90)
-percent_btn.place(x=85, y=90)
-equal_btn.place(x=215, y=270)
-konma_btn.place(x=150, y=270)
-plus_btn.place(x=215, y=90)
-minus_btn.place(x=215, y=180)
-multiplication_btn.place(x=215, y=135)
-divide_btn.place(x=215, y=225)
+        value = len(self.write_entry.get()) - 1 #終わりの1つ前
+        self.write_entry.delete(value, CTk.END)
 
-app.mainloop()
+        self.write_entry.configure(state="readonly")
+
+    def AllClear(self):
+        self.write_entry.configure(state="normal")
+
+        self.write_entry.delete(0, CTk.END)
+
+        self.write_entry.configure(state="readonly")
+
+    def percent_calculate(self):
+        self.write_entry.configure(state="normal")
+
+        value = float(self.write_entry.get())
+        ans   = value / 100
+        
+        self.write_entry.delete(0, CTk.END)
+        self.write_entry.insert(0, ans)
+
+        self.write_entry.configure(state="readonly")
+
+    def write(self, text: str):
+        self.write_entry.configure(state="normal")
+
+        self.write_entry.insert(CTk.END, text)
+
+        self.write_entry.configure(state="readonly")
+
+    def Calculate(self):
+        self.write_entry.configure(state="normal")
+        
+        try:
+            self.write_entry.insert(CTk.END, '=' + str(eval(self.write_entry.get().replace("×", "*").replace("÷", "/"))))
+        except SyntaxError:
+            self.write_entry.delete(first_index=0, last_index=CTk.END)
+
+        self.write_entry.configure(state="readonly")
+
+if __name__ == "__main__":
+    calclator = Calculator()
+    calclator.mainloop()
